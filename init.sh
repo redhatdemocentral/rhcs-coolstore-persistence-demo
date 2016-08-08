a55700b74a18cba2863428a6adf4344ce324935f
Eric D. Schabell <eric@schabell.org>
2016-08-08 13:25:33 -0400

    Added updates to bpm suite 6.3.

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@ PROJECT="git@github.com:redhatdemocentral/rhcs-coolstore-persistence-demo.git"
 SRC_DIR=./installs
 OPENSHIFT_USER=openshift-dev
 OPENSHIFT_PWD=devel
-BRMS=jboss-brms-installer-6.2.0.GA-redhat-1-bz-1334704.jar
+BRMS=jboss-brms-6.3.0.GA-installer.jar
 EAP=jboss-eap-6.4.0-installer.jar
-EAP_PATCH=jboss-eap-6.4.4-patch.zip
+EAP_PATCH=jboss-eap-6.4.7-patch.zip
 
 # wipe screen.
 clear 
@@ -109,6 +109,9 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
+# need to wait a bit for new build to finish with developer image.
+sleep 3 
+
 echo
 echo "Importing developer image..."
 echo
@@ -123,7 +126,7 @@ fi
 echo
 echo "Starting a build, this takes some time to upload all of the product sources for build..."
 echo
-oc start-build rhcs-coolstore-p-demo --from-dir=. --follow=true
+oc start-build rhcs-coolstore-p-demo --from-dir=. --follow=true --wait=true
 
 if [ $? -ne 0 ]; then
 	echo
